31300b1e20cb7ab9d9714a33e05964696205c01a
RuslanGon <ruslan441006@gmail.com>
2024-08-11 17:07:37 +0300

    wer

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+export {
+    capitalizeFirstLetter,
+    addSpaceBeforeUnit,
+    camelCaseToSentenceCase,
+  } from './formatString';
+  export { modalContext, useModal } from './createModalContext';
+  export { createModalFeaturesList } from './createModalFeaturesList';
+  export { createPageFeaturesList } from './createPageFeaturesList';
+  export { createVehicleDetailsList } from './createVehicleDetailsList';
+  export { formatDate } from './formatDate';
+  export { bookingFormValidationSchema } from './validationSchema';
